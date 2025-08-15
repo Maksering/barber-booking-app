b8bba700722daa5bf884d905e2d943a34f465b26
Maksering <maksering.off@gmail.com>
2025-08-15 22:16:18 +0300

    Change docker-compose and init.sql Now docker on start create table in db

--- docker-project/postgres/init.sql
+++ docker-project/postgres/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE public."user" (
-                               id BIGSERIAL PRIMARY KEY,
-                               email VARCHAR(255),
-                               password VARCHAR(255),
-                               name VARCHAR(255),
-                               phone VARCHAR(255),
-                               role VARCHAR(10) CHECK (role IN ('CLIENT', 'MASTER', 'ADMIN'))
+CREATE TABLE IF NOT EXISTS users(
+                                    id BIGSERIAL PRIMARY KEY,
+                                    email VARCHAR(255),
+                                    password VARCHAR(255),
+                                    name VARCHAR(255),
+                                    phone VARCHAR(255),
+                                    role VARCHAR(10) CHECK (role IN ('CLIENT', 'MASTER', 'ADMIN'))
 );
 
-COMMENT ON COLUMN public."user".role IS 'Possible values: CLIENT, MASTER, ADMIN';
+COMMENT ON COLUMN users.role IS 'Possible values: CLIENT, MASTER, ADMIN';
